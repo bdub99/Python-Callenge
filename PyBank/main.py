b995d270d0ade080bdcd452abab0f8ddeaa6c995
Benjamin Wynne <benjaminwynne@Benjamins-Air.lan>
2022-08-03 13:57:26 -0700

    some progress made

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,42 +4,67 @@
 # The dataset is composed of two columns: "Date" and "Profit/Losses". 
 # (Thankfully, your company has rather lax standards for accounting, so the records are simple.)
 
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
- 
-# The total number of months included in the dataset
-
-# * The net total amount of "Profit/Losses" over the entire period
-
-# * The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-# * The greatest increase in profits (date and amount) over the entire period
-
-# * The greatest decrease in profits (date and amount) over the entire period
-
 import os
 import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-# total_months = []
-# total_PL = {}
-# profit = {}
-# loses =[]
-# profit_inc = []
-# profit_dec = []
-
+total_months = []
+total_PL = []
+average_change = []
+profit_inc = []
+profit_dec = []
 
 with open(csvpath) as csvfile:
     
     csvreader = csv.reader(csvfile, delimiter= ",")
     
-    print(csvreader)
+    csv_header = next(csvreader)
 
-    # Read the header row first (skip this step if there is now header)
-    # csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
+    for row in csvreader:
+        # The total number of months included in the dataset
+        
+        total_months.append(row[1])
+        
+        # * The net total amount of "Profit/Losses" over the entire period
 
-    # # # Read each row of data after the header
-    # for row in csvreader:
-    #     print(row)
+        total_PL.append(int(row[2]))
+
+        # * The changes in "Profit/Losses" over the entire period, and then the average of those changes
+        
+        # budgetdata = round(int(total_months / total_PL) * 100, 2)
+        # average_change.append(str(budgetdata))
+        
+        # * The greatest increase in profits (date and amount) over the entire period
+
+        profit_inc.append(max(int[row(2)]))
+
+        # * The greatest decrease in profits (date and amount) over the entire period
+        
+        profit_dec.append(min(int[row(2)]))
+
+
+print(str(len(total_months)))
+print(str(len(total_PL)))
+# print(str(int(average_change)))
+print(str(int(profit_inc)))
+print(str(int(profit_dec)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
