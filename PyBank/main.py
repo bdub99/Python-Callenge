0ab4f75fc94a25a3f2e093191ac5eba2a8dfbd74
Benjamin Wynne <benjaminwynne@Benjamins-Air.lan>
2022-08-04 10:01:05 -0700

    updated pybank 10am

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,11 +9,11 @@ import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-total_months = []
-total_PL = []
-average_change = []
-profit_inc = []
-profit_dec = []
+total_months = 0
+total_PL = 0
+average_change = 0
+profit_inc = 0
+profit_dec = 0
 
 with open(csvpath) as csvfile:
     
@@ -24,29 +24,29 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         # The total number of months included in the dataset
         
-        total_months.append(row[1])
+        total_months +=1
         
         # * The net total amount of "Profit/Losses" over the entire period
 
-        total_PL.append(int(row[2]))
+        total_PL.append(int(row[1]))
 
         # * The changes in "Profit/Losses" over the entire period, and then the average of those changes
         
-        # budgetdata = round(int(total_months / total_PL) * 100, 2)
-        # average_change.append(str(budgetdata))
+        budgetdata = round(int(total_months / total_PL) * 100, 2)
+        average_change.append(str(budgetdata + 1))
         
         # * The greatest increase in profits (date and amount) over the entire period
 
-        profit_inc.append(max(int[row(2)]))
+        profit_inc.append(max(int[row(1)]))
 
         # * The greatest decrease in profits (date and amount) over the entire period
         
-        profit_dec.append(min(int[row(2)]))
+        profit_dec.append(min(int[row(1)]))
 
 
-print(str(len(total_months)))
-print(str(len(total_PL)))
-# print(str(int(average_change)))
+print(total_months)
+print(str(total_PL))
+print(str(int(average_change)))
 print(str(int(profit_inc)))
 print(str(int(profit_dec)))
 
